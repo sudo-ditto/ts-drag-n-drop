c59e464cd31c57b0488b944362383adb69d1156a
Loretta <loreta.krasteva@brandmaker.com>
2021-10-31 15:16:37 +0200

    Improved form validation

--- src/app.ts
+++ src/app.ts
@@ -11,6 +11,54 @@ function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
   return adjustedDescriptor;
 }
 
+// Form Validation
+interface Validatable {
+  value: string | number;
+  required?: boolean;
+  minLength?: number;
+  maxLength?: number;
+  min?: number;
+  max?: number;
+}
+
+function formValidation(validatableInput: Validatable) {
+  let isValid = true;
+  if (validatableInput.required) {
+    isValid = isValid && validatableInput.value.toString().trim().length !== 0;
+  }
+  if (
+    //   != includes both null and undefined
+    validatableInput.minLength != null &&
+    typeof validatableInput.value === "string"
+  ) {
+    isValid =
+      isValid && validatableInput.value.length >= validatableInput.minLength;
+  }
+  if (
+    //   != includes both null and undefined
+    validatableInput.maxLength != null &&
+    typeof validatableInput.value === "string"
+  ) {
+    isValid =
+      isValid && validatableInput.value.length <= validatableInput.maxLength;
+  }
+  if (
+    //   != includes both null and undefined
+    validatableInput.min != null &&
+    typeof validatableInput.value === "number"
+  ) {
+    isValid = isValid && validatableInput.value >= validatableInput.min;
+  }
+  if (
+    //   != includes both null and undefined
+    validatableInput.max != null &&
+    typeof validatableInput.value === "number"
+  ) {
+    isValid = isValid && validatableInput.value <= validatableInput.max;
+  }
+  return isValid;
+}
+
 // Project Input Class
 class ProjectInput {
   // Template element to render in the DOM.
@@ -64,10 +112,29 @@ class ProjectInput {
     const enteredPeople = this.peopleInputElement.value;
 
     //   validation
+    const titleValidatable: Validatable = {
+      value: enteredTitle,
+      required: true,
+    };
+
+    const descriptionValidatable: Validatable = {
+      value: enteredDescription,
+      required: true,
+      minLength: 5,
+      maxLength: 7,
+    };
+
+    const peopleValidatable: Validatable = {
+      value: +enteredPeople,
+      required: true,
+      min: 1,
+      max: 5,
+    };
+
     if (
-      enteredTitle.trim().length === 0 ||
-      enteredDescription.trim().length === 0 ||
-      enteredPeople.trim().length === 0
+      !formValidation(titleValidatable) ||
+      !formValidation(descriptionValidatable) ||
+      !formValidation(peopleValidatable)
     ) {
       alert("Invalid input, please try again!");
       return;
@@ -77,9 +144,9 @@ class ProjectInput {
   }
 
   private clearInputs() {
-      this.titleInputElement.value = '';
-      this.descriptionInputElement.value = '';
-      this.peopleInputElement.value = '';
+    this.titleInputElement.value = "";
+    this.descriptionInputElement.value = "";
+    this.peopleInputElement.value = "";
   }
 
   @autobind
@@ -90,9 +157,9 @@ class ProjectInput {
 
     const userInput = this.gatherUserInput();
     if (Array.isArray(userInput)) {
-        const [title, desc, people] = userInput;
-        console.log(title, desc, people);
-        this.clearInputs();
+      const [title, desc, people] = userInput;
+      console.log(title, desc, people);
+      this.clearInputs();
     }
   }
 
